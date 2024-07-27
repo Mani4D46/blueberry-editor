c5c10508276c19b67dd5dbbddd3bf429b17aac28
Mani4D46 <119421025+Mani4D46@users.noreply.github.com>
2024-07-27 18:22:22 +0330

    :truck: Renamed files, :poop: written some bad code.

---
+++ src/strawberry/main.py
@@ -0,0 +1,40 @@
+"""
+This file is the main app, it combines all the required parts.
+"""
+import sys
+from . import terminal
+from .terminal import ansi_codes
+
+
+class App():
+    """
+    The blueberry editor app.
+    """
+    def __init__(self):
+        self.tabs = []
+        self.current_tab = None
+        self.state_command_palette = terminal.State(visibility=False)
+
+        self.write = sys.stdout.write
+        self.flush = sys.stdout.flush
+
+    def __enter__(self):
+        self.write(ansi_codes.enable_alternative_screen_buffer())
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.write(ansi_codes.disable_alternative_screen_buffer())
+
+    def update(self):
+        # Do some updates here
+        self.draw()
+        self.flush()
+
+    def draw(self):
+        """
+        Writes everything on the screen before the flush.
+        """
+        # self.draw_menus()
+        # self.draw_tab()
+        # self.draw_current_tab()
+        # if self.state_command_palette.visibility is True:
+        #     self.draw_command_palette()
---
+++ src/strawberry/terminal/state.py
@@ -0,0 +1,17 @@
+"""
+States
+"""
+
+
+class State():
+    """
+    State of everything should be defined in this class.
+
+    Example:
+        >>> some_state = State(visibility=True)
+        >>> some_state.visibility
+        True
+    """
+    def __init__(self, **state):
+        for key, value in state.items():
+            setattr(self, key, value)
