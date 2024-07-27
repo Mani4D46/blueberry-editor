78e2237c7508c90ccce5a6a11ca1dc62e215e4b1
Mani4D46 <119421025+Mani4D46@users.noreply.github.com>
2024-07-27 18:27:02 +0330

    :truck: fixed the wrong init file location

--- src/blueberry/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Here you will find everything related to the terminal I/O.
-"""
-
-from .state import State
-
-__all__ = ['State']
---
+++ src/blueberry/terminal/__init__.py
@@ -0,0 +1,7 @@
+"""
+Here you will find everything related to the terminal I/O.
+"""
+
+from .state import State
+
+__all__ = ['State']
