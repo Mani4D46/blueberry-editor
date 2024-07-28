e634f04a9ad72160a74b331f93d6214b72a67f3b
Mani4D46 <119421025+Mani4D46@users.noreply.github.com>
2024-07-28 06:43:42 +0330

    :thread: Added code for multi-threading of I/O

--- src/blueberry/main.py
+++ src/blueberry/main.py
@@ -2,6 +2,7 @@
 This file is the main app, it combines all the required parts.
 """
 import sys
+import threading
 from . import terminal
 from .terminal import ansi_codes
 
@@ -17,21 +18,46 @@ class App():
 
         # states
         self.state_command_palette = terminal.State(visibility=False)
+        self.is_running = True
 
         # stdout
         self.write = sys.stdout.write
         self.flush = sys.stdout.flush
 
+        # threading
+        self.input_thread = threading.Thread(target=self.keyboard_input)
+        self.update_thread = threading.Thread(target=self.update)
+
     def __enter__(self):
         self.write(ansi_codes.enable_alternative_screen_buffer())
+        self.input_thread.start()
+        self.update_thread.start()
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self.exit()
+
+    def exit(self):
+        self.is_running = False
         self.write(ansi_codes.disable_alternative_screen_buffer())
 
+    def keyboard_input(self):
+        """
+        Thread to get keyboard input from the user.
+        """
+        while self.is_running:
+            a = input()
+            if a == 'q':
+                self.exit()
+
     def update(self):
-        # Do some updates here
-        self.draw()
-        self.flush()
+        """
+        Thread to update the screen (stdout).
+        """
+        while self.is_running:
+            # Do some updates here
+            print('w')
+            self.draw()
+            self.flush()
 
     def draw(self):
         """
