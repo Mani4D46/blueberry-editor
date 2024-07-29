6ec625a9962dd897921c1144e6df4acdad1ce611
Mani4D46 <119421025+Mani4D46@users.noreply.github.com>
2024-07-29 09:26:49 +0330

    :memo: Added docstring and comments to `configs.py`

--- src/blueberry/configs.py
+++ src/blueberry/configs.py
@@ -1,6 +1,11 @@
+"""
+Here you will find the cofiguration options of blueberry editor.
+
+We use the yachalk module for colors.
+"""
 from yachalk import chalk
 
-
+# You could change colors of the UI elements here:
 colors = {
     "menu": chalk.bg_gray.white,
     "menu.selected": chalk.bg_blue.white
